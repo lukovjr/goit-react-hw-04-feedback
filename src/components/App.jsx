138340992c52f1d5e73e6a42f58099f0e61e81ec
Oleksy Lykov <lukov_jr@ukr.net>
2023-05-22 13:58:00 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from "./Section";
 import { FeedbackOptions } from "./FeedbackOptions";
 import { Statistics } from "./Statistics";
@@ -6,39 +6,34 @@ import { Container } from './App.styled';
 import { Notification } from './Notification';
 
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback  = (name) => {
-      this.setState(prev => {
-        return { [name]: prev[name] + 1 };
-      });
+  const onLeaveFeedback = (name) => {
+    if (name === 'good') setGood(prev => prev + 1);
+    if (name === 'neutral') setNeutral(prev => prev + 1);
+    if (name === 'bad') setBad(prev => prev + 1);
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return (good + neutral + bad);
   };
 
-  countPositiveFeedbackPercentage(total) {
+  const countPositiveFeedbackPercentage = (total) => {
     return (
-      total > 0 ? Math.round((this.state.good * 100) / total) : 0
+      total > 0 ? Math.round((good * 100) / total) : 0
     );
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage(total);
+    const total = countTotalFeedback();
+    const positivePercentage = countPositiveFeedbackPercentage(total);
   
     return (
       <Container>
         <Section title="Please leave feedback">
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onLeaveFeedback }/>
+          <FeedbackOptions options={Object.keys({ good, neutral, bad })} onLeaveFeedback={onLeaveFeedback}/>
         </Section>
         <Section title="Statistics">
           {total > 0 ?
@@ -49,5 +44,4 @@ export class App extends Component {
         </Section>
       </Container>
   );
-  }
 };
